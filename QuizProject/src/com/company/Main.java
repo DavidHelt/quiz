898aebb9eabc37b91f445b3397ef450370833f62
DavidHelt <84445693+DavidHelt@users.noreply.github.com>
2022-05-22 14:08:36 +0200

    Update Main.java

--- QuizProject/src/com/company/Main.java
+++ QuizProject/src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
-        //*create a scanner
+       
         Scanner scanner = new Scanner(System.in);
 
     Game t1 = new Game();
